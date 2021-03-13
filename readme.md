e1b74019cd48f4f2a74d3fc86e58f1904e3ae3e2
Philip Kreißel <philip.kreissel@icloud.com>
2021-03-13 19:38:19 +0100

    make it better

--- readme.md
+++ readme.md
@@ -1,17 +1,23 @@
 Still in early development, use with caution
 
+# Infos
+Wit.ai is free, but only allows only files with max-length 10s.
+This script will iteratively try to cut audio of unlimited size into chunks of appropriate size, while (hopefully) respecting word boundaries.
+Then the chunks are sent to wit and a textfile is exported.
+
 # Install:
 
 pip install -r requirements.txt
 
 # Usage:
 
-- Change WIT_KEY
+- Change WIT_KEY to your wit.ai key (free)
 - Put file.mp3 into folder
 - run python transcribe.py
-
 
 # TODO:
 - Empty Cache after run
 - Different File Types
+- Performance Improvements on first split
+- Reuse Info from first split?
 - Files via Command Line Arguments
--- transcribe.py
+++ transcribe.py
@@ -5,12 +5,14 @@ from joblib import Parallel, delayed
 from tqdm import tqdm
 import os
 from keys import WIT_KEY
+import time
 
 
 client = Wit(WIT_KEY)
 
 sound = AudioSegment.from_mp3("file.mp3")
-chunks = split_on_silence(sound, min_silence_len=2000,silence_thresh=-30)
+print("first split round, this might take a while")
+chunks = split_on_silence(sound, min_silence_len=2000,silence_thresh=-30, seek_step=10)
 
 def chunkexport(chunk, num, level):
     if(len(chunk) > 19000):
@@ -20,21 +22,29 @@ def chunkexport(chunk, num, level):
     else:
         chunk.export("cache/chunk{0}.wav".format(num), format="wav")
 
+print("second split round, this will run in parallel")
 Parallel(n_jobs=12)(delayed(chunkexport)(chunk, str(i).zfill(5), 1) for i, chunk in enumerate(tqdm(chunks)))
 
 text = []
 
-for i, file in enumerate(tqdm(sorted(os.listdir("cache")))):
+def wit_file(file):
+    time.sleep(2)
     if not file.endswith('.wav'):
-        continue
-    print(file)
+        return None
     with open("cache/" + file, 'rb') as f:
         try:
             resp = client.speech(f, {'Content-Type': 'audio/wav'})
-            text.append(resp["text"])
+            os.remove("cache/" + file)
+            return resp["text"] + "\n"
         except Exception as e:
+            #print(resp)
             print(e)
+            return None
+
+text = Parallel(n_jobs=6)(delayed(wit_file)(file) for i, file in enumerate(tqdm(sorted(os.listdir("cache")))))
+
+text
 
 output = open("output.txt","a")
-output.writelines(text)
+output.writelines([str for str in text if not str == None])
 output.close()
